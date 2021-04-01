feb94103af1b040cb4fdda2789c2d535f54bc2ef
Vishal Yadav <yadavvi91@gmail.com>
2021-04-01 10:37:50 +0530

    Hardcoded data

--- src/features/air-quality-index/AQI.js
+++ src/features/air-quality-index/AQI.js
@@ -1,7 +1,33 @@
 import React, { useEffect, useState } from "react";
 
 export function AQI() {
-  const [data, setData] = useState([]);
+  const data = [
+    {
+      city: "Mumbai",
+      aqi: 182,
+      category: "Moderate",
+      prevTime: undefined,
+      currTime: 1617251117777,
+      text: "A few seconds ago"
+    },
+    {
+      city: "Delhi",
+      aqi: 300,
+      category: "Poor",
+      prevTime: undefined,
+      currTime: 1617251117777,
+      text: "A few seconds ago"
+    },
+    {
+      city: "Bhubaneswar",
+      aqi: 100,
+      category: "Satisfactory",
+      prevTime: undefined,
+      currTime: 1617251117777,
+      text: "A few seconds ago"
+    }
+  ];
+  // const [data, setData] = useState([]);
   const [historicalData, setHistoricalData] = useState({});
   const ws = new WebSocket("ws://city-ws.herokuapp.com");
   useEffect(() => {
@@ -11,7 +37,7 @@ export function AQI() {
     };
     ws.onmessage = (event) => {
       // listen to data sent from the websocket server
-      const message = JSON.parse(event.data);
+      /* const message = JSON.parse(event.data);
       const newHistoricalData = {};
       const timeStamp = Math.floor(Date.now());
 
@@ -29,7 +55,7 @@ export function AQI() {
       console.log(`Historical Data: ${JSON.stringify(historicalData)}`);
       setData(message);
       setHistoricalData((historicalData[timeStamp] = newHistoricalData));
-      console.log("ABCD");
+      console.log("ABCD"); */
     };
 
     ws.onclose = () => {
@@ -38,5 +64,16 @@ export function AQI() {
     };
   });
 
-  return <div>AQI</div>;
+  return (
+    <div>
+      {data.map((cityData) => (
+        <div>
+          <div>{cityData.city}</div>
+          <div>{cityData.aqi}</div>
+          <div>{cityData.category}</div>
+          <div>{cityData.text}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
